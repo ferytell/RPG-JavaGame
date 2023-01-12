dcd6b1899468cebe196652d36fe6f434eaf9c34d
fery <67243217+ferytell@users.noreply.github.com>
2023-01-12 12:00:14 +0700

    Add files via upload
    adding player animation

--- newGame/src/entity/Player.java
+++ newGame/src/entity/Player.java
@@ -40,9 +40,9 @@ public class Player extends Entity{
 		try {
 			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
 			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
 			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
-			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
 			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
 			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
 			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
@@ -57,27 +57,40 @@ public class Player extends Entity{
 	
 	
 	public void update() {
+		if(keyH.upPressed == true || keyH.rightPressed == true ||
+				keyH.downPressed == true || keyH.leftPressed == true) {
+			
+			if(keyH.upPressed == true) {
+				y -= speed;
+				direction = "up";	
+			}
+			
+			if(keyH.downPressed == true) {
+				y += speed;
+				direction = "down";
+			}
+			
+			if(keyH.leftPressed == true) {
+				x -= speed;
+				direction = "left";
+			}
+			
+			if(keyH.rightPressed == true) {
+				x += speed;
+				direction = "right";
+			}
 
-		if(keyH.upPressed == true) {
-			y -= speed;
-			direction = "up";
+			spriteCounter++;
+			if(spriteCounter > 15) {			// FPS is 60, this will tell how fast animation change (bigger = slower)
+				if(spriteNum == 1) {
+					spriteNum = 2;
+				}
+				else if(spriteNum == 2) {
+					spriteNum = 1;
+				}
+				spriteCounter = 0;
+			}
 			
-			
-		}
-		
-		if(keyH.downPressed == true) {
-			y += speed;
-			direction = "down";
-		}
-		
-		if(keyH.leftPressed == true) {
-			x -= speed;
-			direction = "left";
-		}
-		
-		if(keyH.rightPressed == true) {
-			x += speed;
-			direction = "right";
 		}
 
 		
@@ -93,16 +106,37 @@ public class Player extends Entity{
 		switch(direction) {
 		
 		case "up":
-			image = up1;
+			if (spriteNum == 1) {
+				image = up1;
+			}
+			if (spriteNum == 2) {
+				image = up2;
+			}		
 			break;
 		case "down":
-			image = down1;
+			if (spriteNum == 1) {
+				image = down1;
+			}
+			if (spriteNum == 2) {
+				image = down2;
+			}
 			break;
 		case "left":
-			image = left1;
+			if (spriteNum == 1) {
+				image = left1;
+			}
+			if (spriteNum == 2) {
+				image = left2;
+			}
 			break;
 		case "right":
-			image = right1;
+			if (spriteNum == 1) {
+				image = right1;
+			}
+			if (spriteNum == 2) {
+				image = right2;
+			}
+			
 			
 			break;
 		}
